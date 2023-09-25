1e5cbfa5744da2155c830aaf326f74f292285fa9
MesfinGithinji <meshgithinji@gmail.com>
2023-09-26 01:29:31 +0300

    Exports data into a CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports to CSV"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    api_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(api_url)
+    username = response.json().get('username')
+
+    api_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        user_id)
+    response = get(api_url)
+    todos = response.json()
+    with open('{}.csv'.format(user_id), 'w') as file:
+        for todo in todos:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(user_id, username, todo.get('completed'),
+                               todo.get('title')))
