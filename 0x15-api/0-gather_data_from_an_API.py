a522b71072586530f8505f030cbdde2cfd18f01f
MesfinGithinji <meshgithinji@gmail.com>
2023-09-26 01:22:56 +0300

    Pycode style fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,14 +6,16 @@ import sys
 
 
 if __name__ == "__main__":
-    
+
     api_url = "https://jsonplaceholder.typicode.com/"
 
     user_info = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
 
-    todos = requests.get(api_url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [todo.get("title") for todo in todos if todo.get("completed") is True]
+    completed = [todo.get("title")
+                 for todo in todos if todo.get("completed") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(
         user_info.get("name"), len(completed), len(todos)))
