cd55edd65f08a6c4e032df717fe3f0a5778039d0
MesfinGithinji <meshgithinji@gmail.com>
2023-10-12 14:02:18 -0700

    Reddit API calls

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'My user Agent 1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json().get('data', {})
         sub_count = data.get('subscribers', 0)
